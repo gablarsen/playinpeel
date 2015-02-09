83d1960fa27fe49bb927592a7261b46aff2d5b3a
Gabriel Larsen <gab.larsen@outlook.com>
2015-02-09 07:49:27 -0800

    update sync method

--- app/controllers/dropboxes_controller.rb
+++ app/controllers/dropboxes_controller.rb
@@ -9,6 +9,8 @@ class DropboxesController < ApplicationController
   end
 
   def sync
+    render :text=>params[:challenge] and return unless params[:challenge].nil?
+
   	@result = DropboxHandler.get_activity
   	@result = DropboxHandler.get_facility unless @result.nil?
   	render :text=>!@result.nil?.to_s
