80a0784e96ff407a5ade066d7ce62e1f61146220
jinnahrae <jinnahrae@gmail.com>
2015-02-09 09:32:14 +0800

    update

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 #Bundler.require(*Rails.groups)
-Bundler.require(*Rails.groups(assets: %w(development test)))
+Bundler.require(*Rails.groups(assets: %w(development test production)))
 
 module StarterKit
   class Application < Rails::Application
