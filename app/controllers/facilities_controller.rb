36ba047e8b239af13551d5737e3e1de64eccde61
jinnahrae <jinnahrae@gmail.com>
2015-02-13 21:31:13 +0800

    removed tail sign [[ from search list

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -1,4 +1,8 @@
 class FacilitiesController < ApplicationController
+  skip_authorization_check
+  skip_before_action :authenticate_user!
+  skip_before_filter :verify_authenticity_token
   def show
+    @search_url = state_pages_post_url
   end
 end
